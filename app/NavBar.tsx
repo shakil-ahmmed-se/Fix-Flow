7f6d2dfa5a8860e869ee0a240f579f60d38f246f
shakil-ahmmed-se <shakilahmmed8906@gmail.com>
2024-10-21 12:08:38 +0600

    active navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,20 +1,32 @@
+"use client"
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { FaBug } from "react-icons/fa6";
+import classnames from 'classnames'
 
 const NavBar = () => {
+    const currentPath =  usePathname();
+    // console.log(currentPath)
     const links = [
         { label: 'Dashboard', href: '/'},
         { label: 'Issues', href: '/issues'}
     ]
     return (
         <nav className='flex space-x-10 border-b mb-5 px-5 h-16 items-center'>
-            <Link href={'/'}><FaBug/></Link>
+            <Link href='/'><FaBug/></Link>
             <ul className='flex space-x-10'>
                 {
                     links.map(link => <Link 
                         key={link.href} 
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                        className={classnames(
+                            {
+                                'text-zinc-900' : link.href === currentPath,
+                                'text-zinc-500' : link.href!== currentPath,
+                                'hover: text-zinc-800 transition-colors' : true
+
+                            }
+                        )} 
                         href={link.href}>{link.label}</Link>)
                 }
             
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IssuesPage = () => {
+    return (
+        <div>
+            <h1>Issues Page</h1>
+        </div>
+    );
+};
+
+export default IssuesPage;
